e6b8acc2b2955e9258355425c7475960a521fe1f
princejs446 <princejs446@gmail.com>
2024-10-05 09:04:19 +0530

    Form in flutter on 5/10/2024

---
+++ lib/timepicker.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TimePickerDemo extends StatefulWidget{
+  const TimePickerDemo({super.key});
+  @override  
+  State<TimePickerDemo> createState()=>_TimePickerDemoState();
+}
+
+class _TimePickerDemoState extends State<TimePickerDemo>{
+  late TimeOfDay selectedTime=TimeOfDay.now();
+  Future<void> _selectedTime(BuildContext context)async{
+    final TimeOfDay? picked=await showTimePicker(context: context, initialTime: selectedTime,
+    );
+    if (picked!=null&&picked!=selectedTime){
+      setState(() {
+        selectedTime=picked;
+      });
+    }
+  }
+  @override  
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Time Picker Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Selected time: ${selectedTime.format(context)}',
+            ),
+           const SizedBox(height: 20),
+           ElevatedButton(onPressed: ()=> _selectedTime(context),
+            child: const Text('Selected Time'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
